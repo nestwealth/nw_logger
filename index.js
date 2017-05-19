c17300a3bf2e50c3fd334f82942e370bebb58c95
Richard Pringle <rpring9@gmail.com>
2017-05-19 13:44:49 -0400

    Add linting and initialize index.js.

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const winston = require('winston');
+
+class Logger {
+  contructior(options) {
+
+  }
+}
+
+module.exports = function(options) {
+  if (typeof options === 'string') {
+    return ;
+  }
+}
